f733c94ea60d620f49a5450eb6b6da6df9e8b836
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-05-08 13:25:37 +0200

    Angular
    1ma lezione 08/05/2023

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Todo Manager 2 PRO';
+
+  counter = 0;
+
+  increment(): void{
+    this.counter++;
+  }
+
+  decrement(): void{
+    this.counter--;
+  }
+}
